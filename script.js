ec5d3349ee33cc061d6642eb042bbf6e4471145f
Megan Coppins <meganlcoppins@gmail.com>
2020-02-05 16:51:31 -0700

    variables commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Variables
+// Object with properties 
+var params={
+  length: prompt("How long would you like your password to be?"),
+  upperCase: confirm("Would you like to use uppercase characters in your password?"),
+  lowerCase: confirm("Would you like to use lowercase characters in your password?"),
+  specialChars: confirm("Would you like to use special characters in your password?"),
+  numericChars: confirm("Would you like to use numeric characters"),
+}
+if(params.length<=8 || params.length>=128){
+  prompt("Please enter a password length that is at least 8 characters long and no longer than 128 characters");
+}
+
+console.log(params);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
