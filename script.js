d857e0db39ffaefa7090a4d7446bbf68ea0c029c
Megan Coppins <meganlcoppins@gmail.com>
2020-02-06 17:09:07 -0700

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// variables
+var lowercase="abcdefghijklmnopqrstuvwyz";
+var uppercase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChars= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numericChars= "0123456789";
+
 // Write password to the #password input
 function writePassword() {
-  // Variables
-  var pwUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var pwLowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var pwSpecialChars = "!@#$%^&*()?><,./?"; 
-  var pwNumericChars = "1234567890";
-// Object with properties 
-  var params={
-    length: prompt("How long would you like your password to be?"), 
-    upperCase: confirm("Would you like to use uppercase characters in your password?"),
-    lowerCase: confirm("Would you like to use lowercase characters in your password?"),
+
+// object with properties including prompts/confirms for user input
+  var params = {
+    length: prompt("Choose a password length between 8 and 128 characters long"),
+    uppercase: confirm("Would you like to use uppercase characters in your var password?"),
+    lowercase: confirm("Would you like to use lowercase characters in your password?"),
     specialChars: confirm("Would you like to use special characters in your password?"),
     numericChars: confirm("Would you like to use numeric characters")
   };
-  var password = generatePassword(params);
-  var passwordText = document.querySelector("#password");
+  // setting up password length requirements to a minimum of 8 characters and maximum of 128 characters as well as converting input into a number rather than a string
+      while (
+        params.length <8 ||
+        params.length >128 ||
+        params.length === undefined
+      ) {
 
-  passwordText.value = password;
+        params.length = parseInt(
+          prompt("Choose a password length between 8 and 128 characters long")
+        );
+      }
+        var password = generatePassword(params);
+        var passwordText = document.querySelector("#password");
 
-  console.log(params);
- 
-  
-}
-function generatePassword(options) {
-  if(options.upperCase){
+        passwordText.value = password;
   }
-  return "supersecretpassword";
-} 
+
+// Randomizing character types to be included in password based on what user chose using getRandomValue function 
+  function generatePassword(params) {
+    var password = "";
+
+    for (let i=0; i < params.length; i++) {
+      if (params.uppercase) {
+        password += getRandomValue(uppercase);
+      }
+      if (params.lowercase) {
+        password += getRandomValue(lowercase);
+      }
+      if (params.numericChars) {
+        password += getRandomValue(numericChars);
+      }
+      if (params.specialChars) {
+        password += getRandomValue(specialChars);
+      }
+    }
+// using .slice array method to take selected elements in an array and return them in a new one
+    return password.slice(0, params.length);
+  }
+// function used to randomize what characters are chosen and what order for the password generated
+  function getRandomValue(str) {
+    return str[Math.floor(Math.random() * str.length)];
+  }
